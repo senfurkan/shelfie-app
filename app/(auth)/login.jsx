e044935c21af6cf554ecc1971c21338d870f5ded
frknsn <furkan@ilgisoft.com>
2025-11-29 22:39:06 +0300

    feat: Add Button component using Pressable

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { Link } from "expo-router";
 
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
+import ThemedButton from "../../components/ThemedButton";
 
 const Login = () => {
+
+  const handleSubmit = async () => {
+    console.log("login form submitted")
+  }
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
@@ -13,6 +19,10 @@ const Login = () => {
       <ThemedText title={true} style={styles.title}>
         Login to Your Account
       </ThemedText>
+
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2" }}>Login</Text>
+      </ThemedButton>
 
       <Spacer height={100} />
 
@@ -31,6 +41,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center"
   },
   title: {
     textAlign: "center",
